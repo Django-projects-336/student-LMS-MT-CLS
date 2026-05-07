4125d22b9c3aa1406ca930edd52bdd7afab1ccd2
RatnaTeja9 <ratnateja.ds@gmail.com>
2026-05-07 15:23:39 +0530

    get method

--- student_CRUD/views.py
+++ student_CRUD/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from .models import StudentDetail
+
 
 # Create your views here.
 def homepage(request) :
-        return render(request, "homepage.html")
+        students_list = StudentDetail.objects.all()
+        context  = {
+                "students" : students_list
+        }
+        return render(request, "homepage.html" , context)
 
 def add_student(request) :
         return render(request, "add_student.html")
