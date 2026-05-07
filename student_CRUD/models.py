1e626be5ab9f774b2f12ba21c1bbd8a7e62dc9b1
RatnaTeja9 <ratnateja.ds@gmail.com>
2026-05-07 10:49:00 +0530

    creating Model student-LMS

--- student_CRUD/models.py
+++ student_CRUD/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class StudentDetails(models.Model) :
+        student_name = models.CharField(max_length=100)
+        email = models.EmailField(max_length=50 , unique=True)
+        course = models.CharField(max_length=75)
+        enrollment_date = models.DateField()
