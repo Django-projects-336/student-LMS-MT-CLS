8283812825c6ffc0426e6a2d6ed2336b773778fb
RatnaTeja9 <ratnateja.ds@gmail.com>
2026-05-08 10:46:43 +0530

    form creation

--- student_CRUD/views.py
+++ student_CRUD/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import StudentDetail
+from .forms import StudentForm
+
 
 
 # Create your views here.
@@ -11,4 +13,8 @@ def homepage(request) :
         return render(request, "homepage.html" , context)
 
 def add_student(request) :
-        return render(request, "add_student.html")
+        form = StudentForm()
+        context = {
+                "form" : form
+        }
+        return render(request, "add_student.html" , context)
