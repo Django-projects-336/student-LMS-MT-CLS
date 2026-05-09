2b920b3bc3ddbc7e58a0f4daf4ab479101498773
RatnaTeja9 <ratnateja.ds@gmail.com>
2026-05-09 15:48:29 +0530

    Post API Created

--- student_CRUD/views.py
+++ student_CRUD/views.py
@@ -57,3 +57,12 @@ class StudentListCreateAPI(APIView) :
                 serializer = StudentSerializer(students , many= True)
                 return Response(serializer.data , 
                                 status=status.HTTP_200_OK)
+        
+        def post(self , request) :
+                serializer = StudentSerializer(data=request.data)
+                if  serializer.is_valid() :
+                        serializer.save()
+                        return Response(serializer.data , 
+                                        status=status.HTTP_201_CREATED)
+                return Response(serializer.errors , 
+                                status=status.HTTP_400_BAD_REQUEST)
